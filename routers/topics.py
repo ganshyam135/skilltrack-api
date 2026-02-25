91e5e5dc6943dc85a95a71e90578defa14d72df9
Ganshyam Suthar <ganshyamsuthar03@gmail.com>
2026-02-25 19:03:48 +0530

    Complete Topics CRUD endpoints

--- routers/topics.py
+++ routers/topics.py
@@ -67,7 +67,7 @@ async def create_topic(
     db.commit()
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def get_topics(
+async def read_all_topics(
     db: db_dependency,
     user: user_dependency
 ):
@@ -77,5 +77,65 @@ async def get_topics(
     topics = db.query(Topics).filter(Topics.owner_id == user.id).all()
 
     return topics
+
+@router.get("/{topic_id}", status_code=status.HTTP_200_OK)
+async def get_topics(
+    db: db_dependency,
+    user: user_dependency,
+    topic_id: int = Path(gt=0)
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed")
     
+    topic = db.query(Topics).filter(Topics.id == topic_id, Topics.owner_id == user.id).first()
+
+    if topic is None:
+        raise HTTPException(status_code=404, detail="Topic not found")
+    
+    return topic
+
+@router.put("/{topic_id}", status_code=status.HTTP_200_OK)
+async def update_topic(
+    db: db_dependency,
+    user: user_dependency,
+    topic_id: int = Path(gt=0),
+    topic_request: CreateTopicRequest = None
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    
+    topic = db.query(Topics).filter(Topics.id == topic_id, Topics.owner_id == user.id).first()
+
+    if topic is None:
+        raise HTTPException(status_code=404, detail="Topic not found")
+    
+    skill = db.query(Skills).filter(Skills.id == topic_request.skill_id, Skills.owner_id == user.id).first()
+
+    if skill is None:
+        raise HTTPException(status_code=404, detail="Skill not found")
+    
+    topic.title = topic_request.title
+    topic.description = topic_request.description
+    topic.skill_id = topic_request.skill_id
+
+    db.commit()
+
+@router.delete("/{topic_id}", status_code=status.HTTP_200_OK)
+async def delete_topic(
+    db: db_dependency,
+    user: user_dependency,
+    topic_id: int = Path(gt=0)
+):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    
+    topic = db.query(Topics).filter(Topics.id == topic_id, Topics.owner_id == user.id).first()
+
+    if topic is None:
+        raise HTTPException(status_code=404, detail="Topic not found")
+    
+    db.delete(topic)
+    db.commit()
+    
+
     
