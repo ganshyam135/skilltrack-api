a2748e41b35b911f3191c4dfdf5aed429920323e
Ganshyam Suthar <ganshyamsuthar03@gmail.com>
2026-02-25 14:53:18 +0530

    Implement delete skill endpoint

--- routers/skills.py
+++ routers/skills.py
@@ -26,6 +26,15 @@ class CreateSkillRequest(BaseModel):
     name: str
     description: str | None = None
 
+@router.get("/", status_code=status.HTTP_200_OK)
+async def read_all_skills(
+        db: db_dependency,
+        user: user_dependency
+):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed")
+    return db.query(Skills).filter(Skills.owner_id == user.id).all()
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_skill(
     db: db_dependency,
@@ -59,3 +68,20 @@ async def get_skill(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Skill not found")
     
     return skill
+
+@router.delete("/{skill_id}", status_code=status.HTTP_200_OK)
+async def delete_skill(
+    db: db_dependency,
+    user: user_dependency,
+    skill_id: int = Path(gt=0)
+):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed")
+    
+    skill = db.query(Skills).filter(Skills.id == skill_id, Skills.owner_id == user.id).first()
+
+    if skill is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Skill not found")
+    
+    db.delete(skill)
+    db.commit()
