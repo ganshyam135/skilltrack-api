99548a1fcc62ef4e2517ae512211486e8ef9b539
Ganshyam Suthar <ganshyamsuthar03@gmail.com>
2026-03-12 13:34:26 +0530

    Prepare database configuration for production

--- database.py
+++ database.py
@@ -5,8 +5,15 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
--- redis_client.py
+++ redis_client.py
@@ -1,8 +1,10 @@
 import redis
 import os
 
-redis_client =  redis.Redis(
-    host="redis",
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+
+redis_client = redis.Redis(
+    host=REDIS_HOST,
     port=6379,
     decode_responses=True
 )
